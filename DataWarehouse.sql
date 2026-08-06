39cf35e0a9ae8ca06b62a2610b2167f9ab9b061c
Gabriele Rumi <gabriele.rumi@icubed.it>
2026-08-06 14:40:08 +0200

    Created all the table related to the bronze schema from the ingestions sources

--- DataWarehouse.sql
+++ DataWarehouse.sql
@@ -26,3 +26,55 @@ CREATE TABLE bronze.crm_cust_info(
     cst_gndr VARCHAR(50),
     cst_create_date DATE
 );
+
+CREATE TABLE bronze.crm_prod_info(
+    prod_id INT PRIMARY KEY,
+    prod_key VARCHAR(50),
+    prod_name VARCHAR(50),
+    prod_cost INT,
+    prod_line VARCHAR(50),
+    prod_start_date DATE,
+    prod_end_date DATE
+);
+
+CREATE TABLE bronze.crm_prod_info(
+    prod_id INT PRIMARY KEY,
+    prod_key VARCHAR(50),
+    prod_name VARCHAR(50),
+    prod_cost INT,
+    prod_line VARCHAR(50),
+    prod_start_date DATE,
+    prod_end_date DATE
+);
+
+CREATE TABLE bronze.crm_sales_details(
+    sales_order_number INT,
+    sales_production_key VARCHAR(50),
+    sales_customer_id INT,
+    sales_order_date DATE,
+    sales_ship_date DATE,
+    sales_due_date DATE,
+    sales_sales_amount INT,
+    sales_quantity INT,
+    sales_price INT,
+);
+
+CREATE TABLE bronze.erp_customers(
+    customer_id INT PRIMARY KEY,
+    customer_birth_date DATE,
+    customer_gender VARCHAR(50)
+  
+);
+
+CREATE TABLE bronze.erp_locations(
+    location_id INT PRIMARY KEY,
+    location_county VARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_item(
+    item_id INT PRIMARY KEY,
+    item_category VARCHAR(50),
+    item_subcategory VARCHAR(50),
+    item_maintenance VARCHAR(50)
+
+);
